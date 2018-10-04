0a4ab7f66dae23277f116260acdc729960139972
danazargarova <dzargarova6105@bths.edu>
2018-10-04 14:46:33 -0400

    message

--- src/DZargarovaLib.java
+++ src/DZargarovaLib.java
@@ -2,4 +2,14 @@ public class DZargarovaLib {
     public static void test(){
         System.out.println("Hello");
     }
+
+    public static boolean isPalindrome(String word){
+        int n= word.length();
+        for (int i=0; i<(n/2); i++) {
+            if (word.charAt(i) != word.charAt(n - i-1))
+                return false;
+        }
+        return true;
+    }
 }
+
--- src/TestFiles.java
+++ src/TestFiles.java
@@ -1,5 +1,7 @@
 public class TestFiles {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         DZargarovaLib.test();
+        DZargarovaLib.isPalindrome("tacocat");
+        System.out.println(DZargarovaLib.isPalindrome("qwertyuiopoiuytrewq"));
     }
 }
