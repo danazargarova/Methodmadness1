94cb466c3bf00f9f0cd967ebd3291877bb0ad25b
danazargarova <dzargarova6105@bths.edu>
2018-10-08 00:06:06 -0400

    finally!!!

--- src/DZargarovaLib.java
+++ src/DZargarovaLib.java
@@ -1,15 +1,78 @@
 public class DZargarovaLib {
+
     public static void test(){
-        System.out.println("Hello");
+        System.out.println("Hello, Mr. Levin");
+        //just to check how to use testfiles
     }
 
     public static boolean isPalindrome(String word){
+        /**
+         * input is a word
+         * output is a boolean: true if palindrome, false if not palindrome
+         */
         int n= word.length();
         for (int i=0; i<(n/2); i++) {
             if (word.charAt(i) != word.charAt(n - i-1))
+                //goes thru letter at each i value to check if it matches the corresponding letter on the other half of the word
                 return false;
         }
         return true;
     }
-}
+    public static String dateStr(String date) {
+        /**
+         * input is a date in the format mm/dd/yy
+         * output is a date in the format dd-mm-yy
+         */
+        String month = date.substring(0, 2);
+        String day= date.substring(3,5);
+        String year= date.substring(6,date.length());
+        return (day + "-" + month + "-" + year);
+    }
+
+    public static String cutOut(String mainStr, String subStr){
+        /**
+         * mainStr is the word that gets cut
+         * subStr is the word that gets removed from mainStr
+         * output is new string consisting of mainStr with the subStr removed
+         */
+        String newStr1= mainStr.substring(0,mainStr.indexOf(subStr)); //first part of mainStr before subStr
+        int length= newStr1.length()+subStr.length(); // index of the last letter in subStr
+        String newStr2=mainStr.substring(length, mainStr.length()); // last part of mainStr after subStr
+        return newStr1 + newStr2; //adds the two strings to make the outputs
+    }
+
+    public static boolean isFibbnacci(int num){
+        /**
+         * input is a number
+         * output is true if number is fibonacci, false if not
+         */
+        int a=0;
+        int b=0;
+        for  (int c=1; c <= num; c= a+b) {
+            a = b;
+            b = c;
+            // finds fibonacci number closest to num
+
+            if (num == c)
+                return true;
+        }
+            return false;
+        }
+
+    public static int sumUpTo(int num){
+        /**
+         * input is an integer
+         * output is sum of all integers from 0 to input
+         */
+        int sum = 0;
+        int n = 1;
+        while(n<=num) {
+            sum += n;
+            n++;
+            // adds all numbers from 0 to input to variable sum
+        }
+        return sum;
+        }
+    }
+
 
--- src/TestFiles.java
+++ src/TestFiles.java
@@ -1,7 +1,20 @@
 public class TestFiles {
     public static void main(String[] args) {
         DZargarovaLib.test();
+
         DZargarovaLib.isPalindrome("tacocat");
         System.out.println(DZargarovaLib.isPalindrome("qwertyuiopoiuytrewq"));
+
+        DZargarovaLib.dateStr("10/07/2018");
+        System.out.println(DZargarovaLib.dateStr("10/07/2018"));
+
+        DZargarovaLib.cutOut("catatonic cat", "cat");
+        System.out.println(DZargarovaLib.cutOut("funky cat","cat"));
+
+        DZargarovaLib.isFibbnacci(9);
+        System.out.println(DZargarovaLib.isFibbnacci(9));
+
+        DZargarovaLib.sumUpTo(4);
+        System.out.println(DZargarovaLib.sumUpTo(5));
     }
 }
